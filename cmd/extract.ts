9759ddde5cdcd167b3b33be1c0f16a43a3bf5325
awtnb <arikaebanataw@gmail.com>
2026-01-28 22:27:18 +0900

    add extract sub command

--- cmd/extract.ts
+++ cmd/extract.ts
@@ -1,2 +1,77 @@
 import { Command } from "@cliffy/command";
-export const extractCommand = new Command();
+import { PDFPage, PDFDocument } from "pdf-lib";
+import { sprintf } from "@std/fmt/printf";
+
+const withSuffix = (path: string, suffix: string): string => {
+  const parts = path.split(".");
+  const extension = parts.pop() || "pdf";
+  return parts.join(".") + suffix + "." + extension;
+};
+
+const extractPages = async (
+  path: string,
+  fromIdx: number,
+  toIdx: number,
+): Promise<number> => {
+  const data = await Deno.readFile(path);
+  const srcDoc = await PDFDocument.load(data);
+  const outDoc = await PDFDocument.create();
+  const pCount = srcDoc.getPageCount();
+
+  if (fromIdx < 0) {
+    fromIdx = pCount + fromIdx;
+  }
+  if (toIdx < 0) {
+    toIdx = pCount + toIdx;
+  }
+  const range = srcDoc.getPageIndices().filter((idx: number) => {
+    return fromIdx <= idx && idx <= toIdx;
+  });
+  if (range.length < 1) {
+    console.error("invalid range!");
+    return 1;
+  }
+  const pages = await outDoc.copyPages(srcDoc, range);
+
+  pages.forEach((page: PDFPage) => {
+    outDoc.addPage(page);
+  });
+  const bytes = await outDoc.save();
+  const suf = sprintf("_p%03d-p%03d", fromIdx + 1, toIdx + 1);
+  const outPath = withSuffix(path, suf);
+  await Deno.writeFile(outPath, bytes);
+  return 0;
+};
+
+const nombreToPageIndex = (nombre: number): number => {
+  if (0 < nombre) {
+    return nombre - 1;
+  }
+  return nombre;
+};
+export const extractCommand = new Command()
+  .description("extract page(s) from pdf file.")
+  .arguments("<path:string>")
+  .option("-f, --from <from:integer>", "start of extract (1-origin)", {
+    default: 1,
+  })
+  .option(
+    "-t, --to <to:integer>",
+    "end of extract (1-origin, -1 is last page)",
+    {
+      default: -1,
+    },
+  )
+  .action(async (options, path) => {
+    const fromIdx = nombreToPageIndex(options.from);
+    const toIdx = nombreToPageIndex(options.to);
+
+    const result = await extractPages(path, fromIdx, toIdx);
+
+    if (result !== 0) {
+      console.error("Failed to extract!");
+      Deno.exit(result);
+    }
+
+    console.log(`Extracted: ${path} pp.${options.from}-${options.to}`);
+  });
