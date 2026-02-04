29615c867dd0a69909b44b9e4526e5fb023731cf
awtnb <arikaebanataw@gmail.com>
2026-02-04 22:56:37 +0900

    add files

---
+++ cmd/trim.ts
@@ -0,0 +1,3 @@
+import { Command } from "@cliffy/command";
+// https://github.com/AWtnb/deno-pdf-trim-margin/blob/main/main.ts
+export const trimboxfyCommand = new Command();
---
+++ cmd/unzip.ts
@@ -0,0 +1,3 @@
+import { Command } from "@cliffy/command";
+// https://github.com/AWtnb/deno-pdf-unzip/blob/main/main.ts
+export const unspreadCommand = new Command();
