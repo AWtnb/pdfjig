1b2e05ddb4646ecefcf87e52b5cf196670b9f6f6
awtnb <arikaebanataw@gmail.com>
2026-02-05 23:05:39 +0900

    add applytrimbox command

--- cmd/applytrimbox.ts
+++ cmd/applytrimbox.ts
@@ -1,3 +1,36 @@
 import { Command } from "@cliffy/command";
-// https://github.com/AWtnb/deno-pdf-apply-trimbox/blob/main/main.ts
-export const applyTrimboxCommand = new Command();
+import { PDFDocument, PDFPage, rectanglesAreEqual } from "pdf-lib";
+import { withSuffix } from "../helper.ts";
+
+const applyTrimbox = async (path: string): Promise<string> => {
+  const data = await Deno.readFile(path);
+  const srcDoc = await PDFDocument.load(data);
+  const outDoc = await PDFDocument.create();
+  const range = srcDoc.getPageIndices();
+  const pages = await outDoc.copyPages(srcDoc, range);
+
+  pages.forEach((page: PDFPage, idx: number) => {
+    const mbox = page.getMediaBox();
+    const tbox = page.getTrimBox();
+    if (rectanglesAreEqual(mbox, tbox)) {
+      console.log(`UNCHANGED: page ${idx + 1} has no trimbox.`);
+    } else {
+      page.setMediaBox(tbox.x, tbox.y, tbox.width, tbox.height);
+      page.setCropBox(tbox.x, tbox.y, tbox.width, tbox.height);
+      page.setBleedBox(tbox.x, tbox.y, tbox.width, tbox.height);
+    }
+    outDoc.addPage(page);
+  });
+  const bytes = await outDoc.save();
+  const outPath = withSuffix(path, "_trimbox");
+  await Deno.writeFile(outPath, bytes);
+  return outPath;
+};
+
+export const applyTrimboxCommand = new Command()
+  .description("apply trimbox to each page of a pdf.")
+  .arguments("<path:string>")
+  .action(async (_, path) => {
+    const result = await applyTrimbox(path);
+    console.log(`Applied trimbox: ${result}`);
+  });
